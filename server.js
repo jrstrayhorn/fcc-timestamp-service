402e1e4e8d0e048f086d7441558e231083cad5b1
JR Strayhorn <jrstrayhorn@gmail.com>
2017-08-10 11:43:18 -0400

    Working on using moment js to parse time parameter

--- server.js
+++ server.js
@@ -5,16 +5,48 @@
 var express = require('express');
 var app = express();
 
+var moment = require('moment');
+
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+app.post('/', function (req, res) {
+  res.json(createJSON(null, null))
+})
+
+app.post('/:time', function(req, res) {
+  var timeParam = req.params.time
+  var timeMoment = moment(timeParam)
+  
+  if (!isNaN(timeParam)) {
+    // need to convert to number
+    timeParam = Number(timeParam)
+    timeMoment = moment().unix(timeParam)
+  }
+  
+  if (timeMoment.isValid) {
+    res.json(createJSON(timeMoment.unix(), timeMoment.format('MMMM D, YYYY')))
+  } else {
+    return res.sendStatus(500).send('Not a valid time string')
+  }
+})
+
+function createJSON(unix, natural) {
+  var results = {}
+  results['unix'] = unix
+  results['natural'] = natural
+  return results
+}
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+
 
 app.get("/dreams", function (request, response) {
   response.send(dreams);
@@ -34,6 +66,6 @@ var dreams = [
 ];
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || '3939', function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
